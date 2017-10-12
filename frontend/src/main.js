345af61b60d565a7e068c94aa22824993956b82b
xanyd <xuanyd@aliyun.com>
2017-10-12 17:51:45 +0800

    init

---
+++ frontend/src/main.js
@@ -0,0 +1,36 @@
+import babelpolyfill from 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+import routes from './routes'
+
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  //NProgress.start();
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')

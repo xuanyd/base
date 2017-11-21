e75706369d84b0aeb2ee9824c233e5063fedb9c5
xanyd <xuanyd@aliyun.com>
2017-11-21 19:36:31 +0800

    test

---
+++ frontend_ng/src/component/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class AppComponent {
+  title = 'login works!';
+}

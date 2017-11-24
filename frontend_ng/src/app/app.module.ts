057b422d112192e0a0d6b9fbd2b32f4224000af9
xanyd <xuanyd@aliyun.com>
2017-11-24 16:13:59 +0800

    init ng-bootstrap

--- frontend_ng/src/app/app.module.ts
+++ frontend_ng/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    NgbModule.forRoot(),
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]

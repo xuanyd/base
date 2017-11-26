3228ea28a5aedbc5407c46138f81e684392fc8ad
xuanyd <xuanyd@aliyun.com>
2017-11-26 22:52:52 +0800

    'init'

--- frontend_ng/src/app/home/home.component.ts
+++ frontend_ng/src/app/home/home.component.ts
@@ -3,7 +3,11 @@ import { Router } from '@angular/router';
 import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'c-login',
+  selector: 'c-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+export class HomeComponent implements OnInit {
+	
+}

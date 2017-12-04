b8d01d56c81c48003c3ffe830f2465ae734ffb3c
xuanyd <xuanyd@aliyun.com>
2017-12-04 22:07:17 +0800

    'init'

--- frontend_ng/src/app/notice/sitecfg.component.ts
+++ frontend_ng/src/app/notice/sitecfg.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'c-user',
+  selector: 'c-sitecfg',
   template: '<router-outlet></router-outlet>'
 })
 export class SiteCfgComponent  {

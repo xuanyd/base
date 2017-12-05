4542c9afb11af6da69fe1f965ec43b97f21a96e1
xanyd <xuanyd@aliyun.com>
2017-12-05 20:13:39 +0800

    p

--- py/OGL/hello.py
+++ py/OGL/hello.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-'a test module'
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
 
-from OpenGL.GL import * 
+def drawFunc():
+    glClear(GL_COLOR_BUFFER_BIT)
+    #glRotatef(1, 0, 1, 0)
+    glutWireTeapot(0.5)
+    glFlush()
+
+glutInit()
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+glutInitWindowSize(400, 400)
+#参数为b类型而不是string
+glutCreateWindow(b"First")
+glutDisplayFunc(drawFunc)
+#glutIdleFunc(drawFunc)
+glutMainLoop() 

8edf6216e0810b6f0aadd90944002029b40e38ca
xanyd <xuanyd@aliyun.com>
2017-12-11 19:55:24 +0800

    'init'

--- frontend_ng/src/app/common/left/left.component.ts
+++ frontend_ng/src/app/common/left/left.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { LocalStorage } from '../storage/local.storage'
+import { MenuData } from '../models/main-model.ts'
 
 /**
  * 左侧菜单组件
@@ -15,6 +16,32 @@ export class LeftComponent implements OnInit {
 	 
   treeMenu: string='treeview'
   userName: string=''
+  menuData:[{
+    "id": "1",
+    "parentId": "0",
+    "name": "主页"
+  },{
+    "id": "2",
+    "parentId": "0",
+    "name": "网站管理",
+    "children": [{
+      "id": "021",
+      "parentId": "2",
+      "name": "通知公告",
+      "url": '/app/sitecfg/notice'
+    },{
+      "id": "022",
+      "parentId": "2",
+      "name": "中心新闻",
+      "url": '/app/sitecfg/center-news'
+    },{
+      "id": "023",
+      "parentId": "2",
+      "name": "友情链接",
+      "url": '/app/sitecfg/center-news'
+    }]
+  }]
+
   constructor(private router: Router,private ls: LocalStorage) {
     this.userName = ls.getObject('username')
   }

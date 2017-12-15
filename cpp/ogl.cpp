53e30f68ea2e175aa1fef40ced5c4b8e71ae3590
xuanyd <xuanyd@aliyun.com>
2017-12-15 22:51:43 +0800

    init notice

--- cpp/ogl.cpp
+++ cpp/ogl.cpp
@@ -1,49 +1,15 @@
-#include <GL/glut.h>
-#include <iostream>
-
-void render(void);
-
-void keyboard(unsigned char c, int x, int y);
-
-void mouse(int button, int state, int x, int y);
-
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(640, 480);
-    glutCreateWindow("Simple GLUT Application");
-
-    glutDisplayFunc(render);    
-    glutKeyboardFunc(keyboard);
-    glutMouseFunc(mouse);
-
-    glutMainLoop();
-}
-
-void keyboard(unsigned char c, int x, int y) {
-    if (c == 27) {
-        exit(0);
-    }
-}
-
-void mouse(int button, int state, int x, int y) {
-    if (button == GLUT_RIGHT_BUTTON) {
-        exit(0);
-    }
-}
-
-void render(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLES);
-        glColor3f(1, 0, 0);
-        glVertex2f(-0.5, -0.5);
-        glColor3f(0, 1, 0);
-        glVertex2f(0.5, -0.5);
-        glColor3f(0, 0, 1);
-        glVertex2f(0.0, 0.5);
-    glEnd();
-
-    glutSwapBuffers();
+#include<windows.h>
+#include <GL/gl.h>
+#include <GL/glaux.h>
+ 
+void main(void) {
+  auxInitDisplayMode(AUX_SINGLE|AUX_RGBA);
+  auxInitPosition(0,0,500,500);
+  auxInitWindow("str");
+  glClearColor(0.0,0.0,0.0,0.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(1.0,0.0,0.0);
+  glRectf(-0.5,-0.5,0.5,0.5);
+  glFlush();
+  _sleep(1000);
 }

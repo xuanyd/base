f60c73ac3147bc8d3ec3d61f92e5bb23c47be762
xanyd <xuanyd@aliyun.com>
2017-12-30 15:23:05 +0800

    'init'

--- server/src/main/java/com/base/admin/controller/AdminController.java
+++ server/src/main/java/com/base/admin/controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Map;
-
+import com.core.util.WebToken;
 @Controller
 class AdminController {
 
@@ -17,6 +17,12 @@ class AdminController {
     @RequestMapping({"admin/login"})
     public @ResponseBody Map login(@RequestParam("username") String username, @RequestParam("password") String password) throws Exception{
         Map<String,Object> retMap = adminService.adminLogin(username, password);
+        if (retMap.get('flag') == "success") {
+            Map<String, Object> loginTokenMap = new HashMap<>();
+            loginTokenMap.put("username", username);
+            loginTokenMap.put("password", password);
+            String token = WebToken.createJWT("adminLogin",TokenMgr.generalSubject(loginTokenMap),1000);
+        }
         return retMap;
     }
 
---
+++ server/src/main/java/com/core/util/WebToken.java
@@ -0,0 +1,39 @@
+package com.core.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+//http://blog.csdn.net/csdn_blog_lcl/article/details/73485463
+//https://github.com/wangcantian/SecurityCommDemo
+public class WebTocken {
+
+	public static String createJWT(String id, String subject, long ttMillis) {
+		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+		long nowMillis = System.currentTimeMillis();
+		Date now = new Date(nowMillis);
+		SecretKey secretKey = generalKey();
+		JwtBuilder builder = Jwts.builder()
+				.setId(id)
+				.setSubject(subject)
+				.setIssuedAt(now)
+				.signWith(signatureAlgorithm, secretKey);
+		if (ttlMillis >= 0) {
+			long expMillis = nowMillis + ttlMillis;
+			Date expDate = new Date(expMillis);
+			builder.setExpiration(expDate);
+		}
+		return builder.compact();
+	}
+
+	public static void main(String[] args) {
+	}
+}

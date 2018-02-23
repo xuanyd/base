f9546fe45f89abc359eb7215a6242f5392754a56
xanyd <xuanyd@aliyun.com>
2018-02-23 18:13:36 +0800

    'i'

--- mobile/src/app/main/main.component.ts
+++ mobile/src/app/main/main.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css','./main.component_01.css']
 })
 export class MainComponent implements OnInit{
   title = 'app';
